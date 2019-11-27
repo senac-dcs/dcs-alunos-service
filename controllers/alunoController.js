f4139a2ea971083d302e8b894927af8f9c7a8068
Bruna K P Reque <bklassmann@gmail.com>
2019-11-26 22:25:37 -0300

    Added try-catch

--- controllers/alunoController.js
+++ controllers/alunoController.js
@@ -1,55 +1,69 @@
 let Aluno = require('../models/alunoModel');
 
 exports.all = function (req, res) {
-    Aluno.get(function (err, alunos) {
-        if (err) {
+    try {
+        Aluno.get(function (err, alunos) {
             res.json({
-                status: "erro",
-                message: err,
+                status: "Successo",
+                message: "Listagem executada com sucesso!",
+                data: alunos
             });
-        }
-        res.json({
-            status: "successo",
-            message: "Listagem executada com sucesso!",
-            alunos: alunos
         });
-    });
+    } catch (err) {
+        res.json({
+            status: "Erro",
+            message: err,
+        });
+    }
 };
 
 exports.new = function (req, res) {
-    var aluno = new Aluno();
-    console.log(req.body);
-    aluno.matricula = req.body.matricula;
-    aluno.name = req.body.name ? req.body.name : aluno.name;
-    aluno.email = req.body.email;
-    aluno.cpf = req.body.cpf;
-    aluno.telefone = req.body.telefone;
-    aluno.data_nascimento = req.body.data_nascimento;
-    aluno.cursos = req.body.cursos;
-    
-    aluno.save(function (err) {
-    res.json({
-            message: 'Aluno criado!',
-            data: aluno
+    try {
+        var aluno = new Aluno();
+     
+        aluno.matricula = req.body.matricula;
+        aluno.name = req.body.name ? req.body.name : aluno.name;
+        aluno.email = req.body.email;
+        aluno.cpf = req.body.cpf;
+        aluno.telefone = req.body.telefone;
+        aluno.data_nascimento = req.body.data_nascimento;
+        aluno.cursos = req.body.cursos;
+
+        aluno.save(function (err) {
+            res.json({
+                status: "Sucesso",
+                message: "Aluno criado com sucesso!",
+                data: aluno
+            });
         });
-    });
+    } catch (err) {
+        res.json({
+            status: "Erro",
+            message: err,
+        });
+    }
 };
 
 exports.view = function (req, res) {
-    Aluno.findById(req.params.aluno_id, function (err, aluno) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'Carregando detalhes...',
-            data: aluno
+    try {
+        Aluno.findById(req.params.aluno_id, function (err, aluno) {
+            res.json({
+                status:"Sucesso",
+                message: 'Carregando detalhes...',
+                data: aluno
+            });
         });
-    });
+    } catch (err) {
+        res.json({
+            status: "Erro",
+            message: err,
+        });
+    }
 };
 
 exports.update = function (req, res) {
-    Aluno.findById(req.params.aluno_id, function (err, aluno) {
-        if (err)
-            res.send(err);
+    try {
+        Aluno.findById(req.params.aluno_id, function (err, aluno) {
             aluno.matricula = req.body.matricula;
             aluno.name = req.body.name ? req.body.name : aluno.name;
             aluno.email = req.body.email;
@@ -58,28 +72,37 @@ exports.update = function (req, res) {
             aluno.data_nascimento = req.body.data_nascimento;
             aluno.cursos = req.body.cursos;
 
-        aluno.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Dados atualizados!',
-                data: aluno
+            aluno.save(function (err) {
+                res.json({
+                    status: "Sucesso",
+                    message: 'Dados atualizados!',
+                    data: aluno
+                });
             });
         });
-    });
+    } catch (err) {
+        res.json({
+            status: "Erro",
+            message: err,
+        });
+    }
 };
 
 exports.delete = function (req, res) {
-    Aluno.remove({
-        _id: req.params.aluno_id
-    }, function (err) {
-        if (err)
-            res.send(err);
-        res.json({
-                status: "successo",
+    try {
+        Aluno.remove({
+            _id: req.params.aluno_id
+        }, function (err) {
+            res.json({
+                status: "Successo",
                 message: 'Deletado!'
             });
         }
-    );
+        );
+    } catch (err) {
+        res.json({
+            status: "Erro",
+            message: err,
+        });
+    }
 };
-
